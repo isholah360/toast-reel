cef14d512e8d5b7b4e83325323e103c21a0702d5
ishola360 <isholah360.com>
2025-02-17 18:19:02 +0100

    mContent

--- components/VideoReel/mainContent.tsx
+++ components/VideoReel/mainContent.tsx
@@ -1,9 +1,10 @@
-'use client';
-
 import React from 'react';
 
+interface MainContentProps {
+  isSidebarOpen: boolean;  // Define the type of isSidebarOpen explicitly
+}
 
-const MainContent = ({ isSidebarOpen }) => {
+const MainContent: React.FC<MainContentProps> = ({ isSidebarOpen }) => {
   return (
     <div className={`ml-64 h-screen flex items-center justify-center bg-black text-white overflow-y-auto ${isSidebarOpen ? 'ml-64' : 'ml-0'} md:ml-64`}>
       <div className="w-full md:w-1/2">
