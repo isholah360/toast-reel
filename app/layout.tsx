af9ee40a5187a7cefd8b260ff9d6dfd2a96565ed
ishola360 <isholah360.com>
2025-02-17 13:13:12 +0100

    childrenMode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client';
 
+import { ReactNode } from 'react';  
 import MobileMenuButton from "@/components/VideoReel/mobileMenu";
 import Sidebar from "@/components/VideoReel/Sidebar";
 import { useState } from "react";
-import '../global.css'
+import '../global.css';
 
-export default function RootLayout({ children }) {
+interface RootLayoutProps {
+  children: ReactNode; 
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <html lang="en">
       <body className="bg-black text-white">
-       
         <MobileMenuButton setIsSidebarOpen={setIsSidebarOpen} />
-        
-      
-        <div className="flex min-h-screen" style={{display:"flex"}}>
-       
+        <div className="flex min-h-screen" style={{ display: "flex" }}>
           <div className="sticky top-0 left-0 h-screen w-1/4 z-10">
             <Sidebar isSidebarOpen={isSidebarOpen} />
           </div>
 
-          
           <div className="flex-1 overflow-y-auto p-4">
             {children}
           </div>
